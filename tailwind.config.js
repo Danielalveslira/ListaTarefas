b9cf6083d838d92df63dab16f409e328f1242e35
Daniel.Dev <dnllira1@gmail.com>
2024-12-21 14:58:13 -0300

    mudando a cor do darkmode e alterando icone de xp

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         "text-color": "#0d1117",
         "button-color": "#3fb950",
         "bg-azul": "#2f81f7",
-        'bg-default': '#C6E7FF', // cor de fundo do modo claro
+        'bg-default': '#FBFBFB', // cor de fundo do modo claro
         'bg-dark': '#0d1117', // cor de fundo do modo escuro
         'text-light': '#1a202c', // cor do texto no modo claro
         'text-dark': '#ffffff', // cor do texto no modo escuro
